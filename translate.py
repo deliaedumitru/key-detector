f9906f056e4e1dbad962a157e40ced9d95cf4890
delia <deliatps@outlook.com>
2017-11-14 15:19:26 -0800

    added scripts for csv conversion and ascii mapping

---
+++ translate.py
@@ -0,0 +1,48 @@
+import sys
+from operator import itemgetter
+csv = open(sys.argv[1], "r")
+name = sys.argv[2]
+txt = open(name, "w")
+
+OFFSET = 33
+
+content = []
+
+# clean unnecessary lines
+for line in csv:
+    if "Note_on_c" in line or "Note_off_c" in line:
+        fields = line.split(", ")
+        content.append(fields)
+
+notes = []
+    
+# find start time & end time for each note    
+for i in range(0, len(content)):
+    if content[i][2] == "Note_on_c":
+        for j in range(i, len(content)):
+            if content[j][2] == "Note_off_c" and content[j][4] == content[i][4]:
+                notes.append((content[i][4], content[i][1], content[j][1]))
+                break
+
+words = {}
+
+# find notes that are being played simultaneously
+for i in range(0, len(notes)):
+    word = chr(int(notes[i][0]) + OFFSET)
+    if notes[i][1] in words:
+        words[notes[i][1]] += word
+    else:
+        words[notes[i][1]] = word
+
+sorted_words = sorted(words.items(), key = lambda x: int(x[0]))
+
+phrase = ""
+for tup in sorted_words:
+    phrase += tup[1] + " "
+
+print(phrase)
+
+
+
+
+
